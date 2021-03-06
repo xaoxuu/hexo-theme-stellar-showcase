f380c828900f13d1d6df36623aef1b866d021315
xaoxuu <xaoxuu@gmail.com>
2021-03-06 22:03:32 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# issues-api
+
+测试地址： https://issues-api.vercel.app/xaoxuu
+
+## 使用方法
+
+[![Deploy to Vercel](https://camo.githubusercontent.com/f209ca5cc3af7dd930b6bfc55b3d7b6a5fde1aff/68747470733a2f2f76657263656c2e636f6d2f627574746f6e)](https://vercel.com/import/project?template=https://github.com/xaoxuu/issues-api)
+
+1. 复制整个仓库，名为 `friends`，然后修改 `_config.yml` 中的信息。
+2. 新建 issue 进行测试
+3. 访问 `https://issues-api.vercel.app/yourname`
+
+如果仓库为其他名称，例如 `examples`，那么需要指定仓库名：
+```
+https://issues-api.vercel.app/yourname/examples
+```
+
+如果仓库默认分支不是 `main`，那么需要指定分支：
+```
+https://issues-api.vercel.app/yourname/examples/master
+```
+
+## 感谢
+
+非常感谢 [@Zfour](https://github.com/Zfour) 的 python 代码和耐心指导！
---
+++ api.py
@@ -0,0 +1,42 @@
+# -*- coding: UTF-8 -*-
+from flask import Flask, jsonify, redirect, make_response
+import requests
+import json
+from bs4 import BeautifulSoup
+import re
+
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    return redirect('https://github.com/xaoxuu/issues-api')
+
+# author: https://github.com/Zfour
+def github_json(owner, repo, branch):
+    source_url = 'https://raw.githubusercontent.com/' + owner + '/' + repo + '/' + branch + '/output.json'
+    req = requests.get(source_url)
+    content = []
+    if req.content:
+        content = json.loads(req.content.decode())
+
+    resp = make_response(jsonify({'code': 0, 'source_url': source_url, 'content': content}))
+    resp.status = '200'
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    resp.headers['Access-Control-Allow-Methods'] = 'PUT,GET,POST,DELETE'
+    resp.headers['Content-Type'] = 'application/json; charset=utf-8'
+    return resp
+
+@app.route('/<owner>', methods=['GET'])
+def start_owner(owner):
+    repo = 'friends'
+    branch = 'main'
+    return github_json(owner, repo, branch)
+
+@app.route('/<owner>/<repo>', methods=['GET'])
+def start_owner_repo(owner, repo):
+    branch = 'main'
+    return github_json(owner, repo, branch)
+
+@app.route('/<owner>/<repo>/<branch>', methods=['GET'])
+def start_owner_repo_branch(owner, repo, branch):
+    return github_json(owner, repo, branch)
